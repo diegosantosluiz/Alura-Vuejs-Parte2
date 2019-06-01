f59c6b36604f1bce035d8e3d1888df5a157353d2
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-05-31 21:08:26 -0300

    Aula 03 - Atividade 01: Exclusão através da API

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 class="centralizado">{{ titulo }}</h1>
+
+    <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
+    
     <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Filtre por parte do titulo" />
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
@@ -46,7 +49,8 @@ export default {
     return {
       titulo: 'Alurapic',
       fotos: [ ],
-      filtro: ''
+      filtro: '',
+      mensagem: ''
     }
   },
 
@@ -63,7 +67,16 @@ export default {
 
   methods: {
     remove(foto) {
-      alert('Remover a foto!' + foto.titulo);
+      this.$http
+        .delete(`http://localhost:3000/v1/fotos/${foto._id}`)
+        .then(() => {
+            this.mensagem = 'Foto removida com sucesso'
+          }, 
+          err => {
+            this.mensagem = 'Não foi possível remover a foto';
+            console.log(err);
+          }
+        )
     }
   },
 
